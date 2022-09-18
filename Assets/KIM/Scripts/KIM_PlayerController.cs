a6f59007badd377a375793465c227e48c3558bbf
Jihoon <21qqq4@gmail.com>
2022-09-18 18:06:09 +0900

    .

--- Assets/KIM/Scripts/KIM_PlayerController.cs
+++ Assets/KIM/Scripts/KIM_PlayerController.cs
@@ -7,7 +7,6 @@ public class KIM_PlayerController : MonoBehaviour
 {
     CharacterController cc;
     GameObject ship;
-    KANG_Move mv;
 
     float yVelocity;
     public float gravity = -9.81f;
@@ -16,6 +15,8 @@ public class KIM_PlayerController : MonoBehaviour
 
     bool isLadder = false;
     bool isModule = false;
+
+    public bool is2P = false;
     public bool IsModule
     {
         get { return isModule; }
@@ -25,77 +26,138 @@ public class KIM_PlayerController : MonoBehaviour
     void Start()
     {
         ship = GameObject.Find("Spaceship");
-        cc = GetComponent<CharacterController>();   
-        mv = ship.GetComponent<KANG_Move>();
+        cc = GetComponent<CharacterController>();  
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        // 로컬로 움직이기 위해 우주선이 이동하는 방향으로 우선 이동
-        LocalMove(KANG_ShipMove.instance.moveDir * KANG_ShipMove.instance.curMoveSpeed);
-        // 사다리에 탔을 때 움직임
-        if (isLadder)
+        if (!is2P)
         {
-            yVelocity = 0f;
-
-            if (Input.GetKey(KeyCode.LeftArrow))
-            {
-                cc.Move(-Vector3.right * speed * Time.deltaTime);
-            }
-            else if (Input.GetKey(KeyCode.RightArrow))
-            {
-                cc.Move(Vector3.right * speed * Time.deltaTime);
-            }
-            else if (Input.GetKey(KeyCode.UpArrow))
-            {
-                cc.Move(Vector3.up * speed * Time.deltaTime);
-            }
-            else if (Input.GetKey(KeyCode.DownArrow))
-            {
-                cc.Move(Vector3.down * speed * Time.deltaTime);
-            }
-        }
-        // 모듈에 착석했을 때 움직임
-        else if (isModule)
-        {
-            if (Input.GetKeyDown(KeyCode.B))
-            {
-                isModule = false;
-            }
-        }
-        // 그외 상황에서의 움직임
-        else
-        {
-            RaycastHit hit;
-            Debug.DrawRay(transform.position, Vector3.down * 0.4f, Color.red);
-            if (!Physics.Raycast(transform.position, Vector3.down, out hit, 0.4f, LayerMask.GetMask("Ship")))
-            {
-                Debug.Log(hit);
-                yVelocity += gravity * Time.deltaTime;
-            }
-            else
+            // 로컬로 움직이기 위해 우주선이 이동하는 방향으로 우선 이동
+            LocalMove(KANG_ShipMove.instance.moveDir * KANG_ShipMove.instance.curMoveSpeed);
+            // 사다리에 탔을 때 움직임
+            if (isLadder)
             {
                 yVelocity = 0f;
-                if (Input.GetKeyDown(KeyCode.Space))
-                    yVelocity = jumpPower;
+
+                if (Input.GetKey(KeyCode.LeftArrow))
+                {
+                    cc.Move(-Vector3.right * speed * Time.deltaTime);
+                }
+                else if (Input.GetKey(KeyCode.RightArrow))
+                {
+                    cc.Move(Vector3.right * speed * Time.deltaTime);
+                }
+                else if (Input.GetKey(KeyCode.UpArrow))
+                {
+                    cc.Move(Vector3.up * speed * Time.deltaTime);
+                }
+                else if (Input.GetKey(KeyCode.DownArrow))
+                {
+                    cc.Move(Vector3.down * speed * Time.deltaTime);
+                }
+            }
+            // 모듈에 착석했을 때 움직임
+            else if (isModule)
+            {
+                if (Input.GetKeyDown(KeyCode.B))
+                {
+                    isModule = false;
+                }
+            }
+            // 그외 상황에서의 움직임
+            else
+            {
+                Debug.DrawRay(transform.position, Vector3.down * 0.3f, Color.red);
+                if (!Physics.Raycast(transform.position, Vector3.down, 0.37f, LayerMask.GetMask("ShipFloor")))
+                {
+                    yVelocity += gravity * Time.deltaTime;
+                }
+                else
+                {
+                    yVelocity = 0f;
+                    if (Input.GetKeyDown(KeyCode.Space))
+                        yVelocity = jumpPower;
+                }
+
+                if (Input.GetKey(KeyCode.LeftArrow))
+                    cc.Move(-Vector3.right * speed * Time.deltaTime);
+                else if (Input.GetKey(KeyCode.RightArrow))
+                    cc.Move(Vector3.right * speed * Time.deltaTime);
+
+                cc.Move(yVelocity * Vector3.up * Time.deltaTime);
             }
 
-            if (Input.GetKey(KeyCode.LeftArrow))
-                cc.Move(-Vector3.right * speed * Time.deltaTime);
-            else if (Input.GetKey(KeyCode.RightArrow))
-                cc.Move(Vector3.right * speed * Time.deltaTime);
-
-            cc.Move(yVelocity * Vector3.up * Time.deltaTime);
+            if (!isModule && canModule && (Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.LeftArrow) ||
+                Input.GetKeyUp(KeyCode.DownArrow) || Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.M)))
+            {
+                isModule = true;
+            }
         }
-
-        if (!isModule && canModule && (Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.LeftArrow) ||
-            Input.GetKeyUp(KeyCode.DownArrow) || Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.M)))
+        else
         {
-            isModule = true;
-        }
+            // 로컬로 움직이기 위해 우주선이 이동하는 방향으로 우선 이동
+            LocalMove(KANG_ShipMove.instance.moveDir * KANG_ShipMove.instance.curMoveSpeed);
+            // 사다리에 탔을 때 움직임
+            if (isLadder)
+            {
+                yVelocity = 0f;
 
-        //Debug.Log(isModule);
+                if (Input.GetKey(KeyCode.A))
+                {
+                    cc.Move(-Vector3.right * speed * Time.deltaTime);
+                }
+                else if (Input.GetKey(KeyCode.D))
+                {
+                    cc.Move(Vector3.right * speed * Time.deltaTime);
+                }
+                else if (Input.GetKey(KeyCode.W))
+                {
+                    cc.Move(Vector3.up * speed * Time.deltaTime);
+                }
+                else if (Input.GetKey(KeyCode.S))
+                {
+                    cc.Move(Vector3.down * speed * Time.deltaTime);
+                }
+            }
+            // 모듈에 착석했을 때 움직임
+            else if (isModule)
+            {
+                if (Input.GetKeyDown(KeyCode.Z))
+                {
+                    isModule = false;
+                }
+            }
+            // 그외 상황에서의 움직임
+            else
+            {
+                Debug.DrawRay(transform.position, Vector3.down * 0.3f, Color.red);
+                if (!Physics.Raycast(transform.position, Vector3.down, 0.37f, LayerMask.GetMask("ShipFloor")))
+                {
+                    yVelocity += gravity * Time.deltaTime;
+                }
+                else
+                {
+                    yVelocity = 0f;
+                    if (Input.GetKeyDown(KeyCode.X))
+                        yVelocity = jumpPower;
+                }
+
+                if (Input.GetKey(KeyCode.A))
+                    cc.Move(-Vector3.right * speed * Time.deltaTime);
+                else if (Input.GetKey(KeyCode.D))
+                    cc.Move(Vector3.right * speed * Time.deltaTime);
+
+                cc.Move(yVelocity * Vector3.up * Time.deltaTime);
+            }
+
+            if (!isModule && canModule && (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.A) ||
+                Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.D) || Input.GetKeyDown(KeyCode.C)))
+            {
+                isModule = true;
+            }
+        }
     }
 
     public void LocalMove(Vector3 dir)
