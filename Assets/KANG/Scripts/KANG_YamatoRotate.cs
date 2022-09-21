94f29aa64a45e230bb33c5b69c8dbdf677cfbd22
Kang-JiHyeon <65221707+Kang-JiHyeon@users.noreply.github.com>
2022-09-21 10:19:39 +0900

    머신마다 SeatPos 추가

--- Assets/KANG/Scripts/KANG_YamatoRotate.cs
+++ Assets/KANG/Scripts/KANG_YamatoRotate.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 // 우주선 중심을 기준으로 회전하고 싶다.
 // 필요속성: 우주선 중심, 회전 속도, 회전 방향
 
-public class KANG_AutoRotate : MonoBehaviour
+public class KANG_YamatoRotate : MonoBehaviour
 {
     // 회전중심
     public Transform spaceship;
