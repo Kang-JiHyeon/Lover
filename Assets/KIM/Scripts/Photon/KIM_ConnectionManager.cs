34279fecd8421added7dd80833bf5b64d5c711d6
Jihoon <35093963+MightyChipmunk@users.noreply.github.com>
2022-09-21 17:36:05 +0900

    커넥트 씬

--- Assets/KIM/Scripts/Photon/KIM_ConnectionManager.cs
+++ Assets/KIM/Scripts/Photon/KIM_ConnectionManager.cs
@@ -8,7 +8,7 @@ public class KIM_ConnectionManager : MonoBehaviourPunCallbacks
 {
     void Start()
     {
-        OnClickConnect();
+        
     }
     public void OnClickConnect()
     {
@@ -48,6 +48,7 @@ public class KIM_ConnectionManager : MonoBehaviourPunCallbacks
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.anyKeyDown)
+            OnClickConnect();
     }
 }
