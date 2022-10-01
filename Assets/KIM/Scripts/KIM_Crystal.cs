4ef25dfa5cf4e62bf1711d87ab35890320f2d5e0
Jihoon <21qqq4@gmail.com>
2022-10-01 21:05:47 +0900

    Update KIM_Crystal.cs

--- Assets/KIM/Scripts/KIM_Crystal.cs
+++ Assets/KIM/Scripts/KIM_Crystal.cs
@@ -20,13 +20,16 @@ public class KIM_Crystal : MonoBehaviour
     {
         if (other.gameObject.layer == LayerMask.NameToLayer("Module"))
         {
+            //맵에 부딪힐때
             if (other.gameObject.name.Contains("Map"))
                 return;
+            //모듈에 부딪힐 때 스프라이트 변경시키고 파괴됨
             if (!gameObject.name.Contains("CrowBar"))
             {
                 other.transform.Find("CrystalPot").GetComponent<SpriteRenderer>().sprite = GetComponentInChildren<SpriteRenderer>().sprite;
                 Destroy(gameObject);
             }
+            //내가 크로우바라면 모듈에 부딪힐 때 스프라이트 변경시키고 자신의 스프라이트도 변경, 동시에 이름도 변경
             else
             {
                 GetComponentInChildren<SpriteRenderer>().sprite = other.transform.Find("CrystalPot").GetComponent<SpriteRenderer>().sprite;
