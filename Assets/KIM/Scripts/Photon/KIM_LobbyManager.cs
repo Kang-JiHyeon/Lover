b3cf0aabab63b0624785db7d250f2edf2b2ced1b
Jihoon <21qqq4@gmail.com>
2022-10-03 20:16:58 +0900

    Update KIM_LobbyManager.cs

--- Assets/KIM/Scripts/Photon/KIM_LobbyManager.cs
+++ Assets/KIM/Scripts/Photon/KIM_LobbyManager.cs
@@ -487,8 +487,8 @@ public class KIM_LobbyManager : MonoBehaviourPunCallbacks
     [PunRPC]
     void RPCLoadScene()
     {
-        //PhotonNetwork.LoadLevel("KIM_Scene_Game 2");
-        PhotonNetwork.LoadLevel("KANG_Scene_Game 4");
+        PhotonNetwork.LoadLevel("KIM_Scene_Game 2");
+        //PhotonNetwork.LoadLevel("KANG_Scene_Game 4");
     }
 
     //¹æ »ý¼º
